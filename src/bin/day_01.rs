13e9376e7791fd82349ddf18e8e3ab3712707ab8
Matt Mullins <mmullins@mmlx.us>
2018-12-04 21:22:10 -0800

    day 01: cycle through the input, solve second half

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -4,15 +4,29 @@ fn main() {
     let stdin = std::io::stdin();
     let lock = stdin.lock();
 
-    let mut frequency = 0;
+    let data: Vec<_> = lock
+        .lines()
+        .map(|line| {
+            line.expect("file should be readable")
+                .parse::<i64>()
+                .expect("file should contain integers")
+        }).collect();
 
-    for line in lock.lines() {
-        let i = line
-            .expect("file should be readable")
-            .parse::<i64>()
-            .expect("file should contain integers");
-        frequency += i;
-    }
+    let frequency: i64 = data.iter().sum();
 
     println!("Total frequency: {}", frequency);
+
+    let seen_twice = data.iter().cycle().try_fold(
+        (std::collections::HashSet::new(), 0),
+        |(mut seen, frequency), i| {
+            let frequency = frequency + i;
+            if !seen.insert(frequency) {
+                Err(frequency)
+            } else {
+                Ok((seen, frequency))
+            }
+        },
+    );
+
+    println!("Seen twice: {}", seen_twice.unwrap_err());
 }
