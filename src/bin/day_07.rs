a7661699920ba6415da5432b3fccf062e6dc63df
Matt Mullins <mmullins@mmlx.us>
2018-12-17 20:12:42 -0800

    day 07: pass the test for topological sort

--- src/bin/day_07.rs
+++ src/bin/day_07.rs
@@ -15,6 +15,34 @@ fn parse_input(input: &[u8]) -> (u8, u8) {
     .1
 }
 
+fn topological_sort(edges: &[(u8, u8)]) -> Vec<u8> {
+    use std::collections::{BTreeSet, HashSet};
+    let mut to_visit: BTreeSet<u8> = edges
+        .iter()
+        .flat_map(|&(from, to)| vec![from, to])
+        .collect();
+    let mut completed: std::collections::HashSet<u8> = HashSet::new();
+    let mut order: Vec<u8> = Vec::new();
+
+    while !to_visit.is_empty() {
+        let this = to_visit
+            .iter()
+            .filter(|&&v| {
+                edges
+                    .iter()
+                    .all(|&(from, to)| to != v || completed.contains(&from))
+            })
+            .next()
+            .unwrap()
+            .clone();
+        completed.insert(this);
+        order.push(this);
+        to_visit.remove(&this);
+    }
+
+    order
+}
+
 #[test]
 fn examples() {
     let input = [
@@ -29,4 +57,6 @@ fn examples() {
     let output: Vec<_> = input.iter().map(|&x| parse_input(x)).collect();
     assert_eq!(output[0], (b'C', b'A'));
     assert_eq!(output[1], (b'C', b'F'));
+
+    assert_eq!(topological_sort(&output), b"CABDFE");
 }
