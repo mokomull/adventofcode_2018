8f8181534cbf281e9d530ffa3c4a20157450cd04
Matt Mullins <mmullins@mmlx.us>
2019-01-02 21:06:50 -0800

    day 11: repeat the search for variable size
    This takes about 4m15s to run on my laptop, which could be better.  But
    this solves part 2!

--- src/bin/day_11.rs
+++ src/bin/day_11.rs
@@ -8,17 +8,27 @@ fn power_level(serial: usize, x: usize, y: usize) -> isize {
     digit as isize - 5
 }
 
-fn find_largest(serial: usize) -> (usize, usize) {
+fn find_largest(serial: usize, size: usize) -> ((usize, usize), isize) {
     use itertools::Itertools;
 
-    (1..=297)
-        .cartesian_product(1..=297)
+    (1..=301 - size)
+        .cartesian_product(1..=301 - size)
         .map(|(x, y)| {
-            let power = (x..x + 3)
-                .cartesian_product(y..y + 3)
+            let power = (x..x + size)
+                .cartesian_product(y..y + size)
                 .map(|(ix, iy)| power_level(serial, ix, iy))
                 .sum::<isize>();
             ((x, y), power)
+        })
+        .max_by_key(|&(_, power)| power)
+        .unwrap()
+}
+
+fn find_largest_variable(serial: usize) -> (usize, usize, usize) {
+    (1..=300)
+        .map(|size| {
+            let ((x, y), power) = find_largest(serial, size);
+            ((x, y, size), power)
         })
         .max_by_key(|&(_, power)| power)
         .unwrap()
@@ -28,9 +38,15 @@ fn find_largest(serial: usize) -> (usize, usize) {
 fn main() {
     let input = std::env::args().nth(1).unwrap();
     let serial = input.parse::<usize>().unwrap();
-    let largest = find_largest(serial);
+    let (largest, _power) = find_largest(serial, 3);
 
     println!("The largest total power is at {},{}", largest.0, largest.1);
+
+    let (x, y, size) = find_largest_variable(serial);
+    println!(
+        "The largest power with variable size is at {},{},{}",
+        x, y, size
+    );
 }
 
 #[test]
@@ -40,6 +56,9 @@ fn example() {
     assert_eq!(power_level(39, 217, 196), 0);
     assert_eq!(power_level(71, 101, 153), 4);
 
-    assert_eq!(find_largest(18), (33, 45));
-    assert_eq!(find_largest(42), (21, 61));
+    assert_eq!(find_largest(18, 3).0, (33, 45));
+    assert_eq!(find_largest(42, 3).0, (21, 61));
+
+    assert_eq!(find_largest_variable(18), (90, 269, 16));
+    assert_eq!(find_largest_variable(42), (232, 251, 12));
 }
