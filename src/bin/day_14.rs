437e005df9e970dd59cfe73fa861b086143c99d3
Matt Mullins <mmullins@mmlx.us>
2019-01-21 12:29:24 -0800

    day 14: use a state machine to search the scores

--- src/bin/day_14.rs
+++ src/bin/day_14.rs
@@ -50,6 +50,30 @@ fn trailing_ten(after: usize) -> [u8; 10] {
     retval
 }
 
+fn search_for(digits: &[u8]) -> usize {
+    let mut found = 0;
+    let find = |digit: u8| -> bool {
+        if digit == digits[found] {
+            found += 1;
+        } else if digit == digits[0] {
+            // e.g. looking for 01245 and scanning through 0101245 cannot ignore
+            // that the second '0' does match.
+            found = 1;
+        } else {
+            found = 0;
+        }
+
+        if found == digits.len() {
+            return true;
+        }
+        false
+    };
+
+    // position() returns the index of the last digit that matched, so
+    // position()-digits.len() is the _index_ of the digit prior to the match.
+    Recipes::new().position(find).unwrap() - digits.len() + 1
+}
+
 fn main() {
     let output = trailing_ten(503761);
     print!("The next ten are ");
@@ -65,4 +89,9 @@ fn examples() {
     assert_eq!(trailing_ten(5), [0, 1, 2, 4, 5, 1, 5, 8, 9, 1]);
     assert_eq!(trailing_ten(18), [9, 2, 5, 1, 0, 7, 1, 0, 8, 5]);
     assert_eq!(trailing_ten(2018), [5, 9, 4, 1, 4, 2, 9, 8, 8, 2]);
+
+    assert_eq!(search_for(&[5, 1, 5, 8, 9]), 9);
+    assert_eq!(search_for(&[0, 1, 2, 4, 5]), 5);
+    assert_eq!(search_for(&[9, 2, 5, 1, 0]), 18);
+    assert_eq!(search_for(&[5, 9, 4, 1, 4]), 2018);
 }
