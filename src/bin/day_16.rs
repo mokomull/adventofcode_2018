3179d463b595742d2d786be199617479d8658d15
Matt Mullins <mmullins@mmlx.us>
2019-07-01 18:24:52 -0700

    day 16: attempt to factor-out a generic int parser
    Except I have no idea how to express trait bounds such that I can
    convince the compiler that T's FromStr::Err is a Debug-able.  Even
    though I only ever call this with integer types.

--- src/bin/day_16.rs
+++ src/bin/day_16.rs
@@ -27,31 +27,47 @@ named!(line(CompleteByteSlice) -> Line,
     alt!(before_or_after | opcode | empty)
 );
 
+fn four_integers<'a, T: std::str::FromStr + Copy>(
+    input: CompleteByteSlice<'a>,
+    separator: &[u8],
+) -> nom::IResult<CompleteByteSlice<'a>, [T; 4], u32> {
+    do_parse!(
+        input,
+        items: ws!(separated_list!(tag!(separator), nom::digit))
+            >> cond_with_error!(items.len() != 4, call!(create_error))
+            >> (items
+                .iter()
+                .map(|x| std::str::from_utf8(x)
+                    .expect("invalid utf8 somehow")
+                    .parse::<T>()
+                    .expect("couldn't parse an integer"))
+                .collect::<Vec<_>>()[..]
+                .try_into()
+                .expect("wrong number of registers"))
+    )
+}
+
 named!(before_or_after(CompleteByteSlice) -> Line,
     do_parse!(
         kind: alt!(tag!(&b"Before: "[..]) | tag!(&b"After:  "[..])) >>
         tag!(&b"["[..]) >>
-        items: ws!(separated_list!(tag!(&b","[..]), nom::digit)) >>
-        cond_with_error!(items.len() != 4, call!(create_error)) >>
+        items: call!(four_integers::<Reg>, b",") >>
         tag!(&b"]"[..]) >>
-        ({
-            let wrapped = items
-                .iter()
-                .map(|x| std::str::from_utf8(x).expect("invalid utf8 somehow").parse::<Reg>().expect("couldn't parse an integer"))
-                .collect::<Vec<_>>()[..]
-                .try_into()
-                .expect("wrong number of registers");
+        (
             match &*kind {
-                &b"Before: " => Line::Before(wrapped),
-                &b"After:  " => Line::After(wrapped),
+                &b"Before: " => Line::Before(items),
+                &b"After:  " => Line::After(items),
                 _ => panic!("parser bug: neither before nore after")
             }
-        })
+        )
     )
 );
 
 named!(opcode(CompleteByteSlice) -> Line,
-    value!(unimplemented!())
+    do_parse!(
+        operations: call!(four_integers::<u64>, b" ") >>
+        (Line::Opcode(operations))
+    )
 );
 
 named!(empty(CompleteByteSlice) -> Line,
