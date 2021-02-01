59dcb9a4b934debfb5671426216889570501f127
Matt Mullins <mmullins@mmlx.us>
2021-01-31 22:37:51 -0800

    day 18: parse input into Vec<Vec<Acre>>
    Two major changes here:
    
      * moved to using &str instead of &[u8] so I could use `line_ending`,
      * use the function form of `alt` so I could use bytes::complete::tag,
        because otherwise the test results in Incomplete(Size(1)) instead of
        finishing the Vec<>s

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,4 +1,6 @@
-use nom::{alt, tag, IResult};
+use nom::bytes::complete::tag;
+use nom::multi::{many1, separated_list1};
+use nom::{IResult, Parser};
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum Acre {
@@ -7,13 +9,18 @@ enum Acre {
     Lumberyard,
 }
 
-fn acre(input: &[u8]) -> IResult<&[u8], Acre> {
-    alt!(
-        input,
-        tag!(b".") => { |_| Acre::Open } |
-        tag!(b"|") => { |_| Acre::Trees } |
-        tag!(b"#") => { |_| Acre::Lumberyard }
-    )
+fn acre(input: &str) -> IResult<&str, Acre> {
+    nom::branch::alt((
+        tag(".").map(|_| Acre::Open),
+        tag("|").map(|_| Acre::Trees),
+        tag("#").map(|_| Acre::Lumberyard),
+    ))(input)
+}
+
+fn area(input: &str) -> Vec<Vec<Acre>> {
+    separated_list1(nom::character::complete::line_ending, many1(acre))(input)
+        .expect("produced a sequence of lines")
+        .1
 }
 
 fn main() {
@@ -26,7 +33,147 @@ mod test {
 
     #[test]
     fn parse_acre() {
-        assert_eq!(acre(b"."), Ok((&b""[..], Acre::Open)));
-        assert!(acre(b"l").is_err());
+        assert_eq!(acre("."), Ok(("", Acre::Open)));
+        assert!(acre("l").is_err());
+    }
+
+    #[test]
+    fn parse_area() {
+        assert_eq!(
+            area(
+                ".#.#...|#.
+.....#|##|
+.|..|...#.
+..|#.....#
+#.#|||#|#|
+...#.||...
+.|....|...
+||...#|.#|
+|.||||..|.
+...#.|..|."
+            ),
+            vec![
+                vec![
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Lumberyard,
+                    Acre::Open
+                ],
+                vec![
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Trees,
+                    Acre::Lumberyard,
+                    Acre::Lumberyard,
+                    Acre::Trees
+                ],
+                vec![
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Open
+                ],
+                vec![
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Lumberyard,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Lumberyard
+                ],
+                vec![
+                    Acre::Lumberyard,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Trees,
+                    Acre::Trees,
+                    Acre::Trees,
+                    Acre::Lumberyard,
+                    Acre::Trees,
+                    Acre::Lumberyard,
+                    Acre::Trees
+                ],
+                vec![
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open
+                ],
+                vec![
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open
+                ],
+                vec![
+                    Acre::Trees,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Trees
+                ],
+                vec![
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Trees,
+                    Acre::Trees,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Open
+                ],
+                vec![
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Lumberyard,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Open,
+                    Acre::Open,
+                    Acre::Trees,
+                    Acre::Open
+                ],
+            ]
+        )
     }
 }
