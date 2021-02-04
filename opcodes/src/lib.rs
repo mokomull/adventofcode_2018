d8ae52b437604608937a367a0b45972312debb7e
Matt Mullins <mmullins@mmlx.us>
2021-02-03 22:59:25 -0800

    opcodes: only index registers when necessary
    For the `seti` instruction, e.g., the first parameter can be any
    integer, not just something within range of the register numbers.  The
    example in day 19 hit this case exactly -- there are six registers, and
    it does a `seti 6 0 2`.

--- opcodes/src/lib.rs
+++ opcodes/src/lib.rs
@@ -34,60 +34,60 @@ pub fn eval_one<const N: usize>(
 ) -> [Reg; N] {
     let [_opcode, source_1_idx, source_2_idx, dest_idx] = instruction;
 
-    let source_1 = before[source_1_idx];
-    let source_2 = before[source_2_idx];
+    let source_1 = before.get(source_1_idx).cloned();
+    let source_2 = before.get(source_2_idx).cloned();
 
     let source_1_imm = source_1_idx as Reg;
     let source_2_imm = source_2_idx as Reg;
 
     let result_value = match opcode {
-        Addr => source_1 + source_2,
-        Addi => source_1 + source_2_imm,
-        Mulr => source_1 * source_2,
-        Muli => source_1 * source_2_imm,
-        Banr => source_1 & source_2,
-        Bani => source_1 & source_2_imm,
-        Borr => source_1 | source_2,
-        Bori => source_1 | source_2_imm,
-        Setr => source_1,
+        Addr => source_1.unwrap() + source_2.unwrap(),
+        Addi => source_1.unwrap() + source_2_imm,
+        Mulr => source_1.unwrap() * source_2.unwrap(),
+        Muli => source_1.unwrap() * source_2_imm,
+        Banr => source_1.unwrap() & source_2.unwrap(),
+        Bani => source_1.unwrap() & source_2_imm,
+        Borr => source_1.unwrap() | source_2.unwrap(),
+        Bori => source_1.unwrap() | source_2_imm,
+        Setr => source_1.unwrap(),
         Seti => source_1_imm,
         Gtir => {
-            if source_1_imm > source_2 {
+            if source_1_imm > source_2.unwrap() {
                 1
             } else {
                 0
             }
         }
         Gtri => {
-            if source_1 > source_2_imm {
+            if source_1.unwrap() > source_2_imm {
                 1
             } else {
                 0
             }
         }
         Gtrr => {
-            if source_1 > source_2 {
+            if source_1.unwrap() > source_2.unwrap() {
                 1
             } else {
                 0
             }
         }
         Eqir => {
-            if source_1_imm == source_2 {
+            if source_1_imm == source_2.unwrap() {
                 1
             } else {
                 0
             }
         }
         Eqri => {
-            if source_1 == source_2_imm {
+            if source_1.unwrap() == source_2_imm {
                 1
             } else {
                 0
             }
         }
         Eqrr => {
-            if source_1 == source_2 {
+            if source_1.unwrap() == source_2.unwrap() {
                 1
             } else {
                 0
